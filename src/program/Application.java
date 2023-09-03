b98f60c7927330ae2e6ea35b844686a158c1648d
Lucas Cerucci <luccerucci@gmail.com>
2023-09-03 00:05:53 -0300

    seller and department

---
+++ src/program/Application.java
@@ -0,0 +1,15 @@
+package program;
+
+import module.entities.Department;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Department amarelo = new Department(8, "preto");
+		
+		System.out.println(amarelo.toString());
+
+	}
+
+}
